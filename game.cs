f08be3fc5fc3fadfcb25ca61be6221cf6f1eccc2
Fabian van Maanen <h.j.j.vanmaanen@students.uu.nl>
2018-05-15 15:01:51 +0200

    pushen omdat het moest

--- game.cs
+++ game.cs
@@ -14,6 +14,19 @@ namespace Template
         public void Tick()
         {
             screen.Clear(0);
+            int l = screen.width / 2 + 80;
+            for (int i = 0; i < 3; i++)
+            {
+                for (double j = 0.0; j < 360; j++)
+                {
+                    double angle = j * Math.PI / 180;
+                    int x = (int)(l + 50 * Math.Cos(angle));
+                    int y = (int)(80 + 50 * Math.Sin(angle));
+                    int Location = x + y * screen.width;
+                    screen.pixels[Location] = 255;
+                }
+                l += 120;
+            }
         }
     }
 }
